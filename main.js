278d22f3061839cd78439357c66b9e380bc87cc9
Gargenziano03 <gargenziano03@gmail.com>
2024-10-01 16:26:14 +0200

    fix

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 
 //stampare i numeri da 1 a 100
-for(let i = 1; 1 < 101; i++){
+for(let i = 1; i <= 100; i++){
     console.log(i);
     
 }
