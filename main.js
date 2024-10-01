248eff9002005fd5a6a3e6ddc3c8d4e2d6af6499
Gargenziano03 <gargenziano03@gmail.com>
2024-10-01 17:13:56 +0200

    add Fizz

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 //stampare i numeri da 1 a 100
 for(let i = 1; i <= 100; i++){
-    console.log(i);
-    
+//se sono multipli di 3 stampi “Fizz”
+    if(i % 3 === 0){
+        console.log('fizz');
+    }else{
+        console.log(i);  
+    }
 }
