1a942a369c015f45857f7fd28227f2e3ec523cba
Gargenziano03 <gargenziano03@gmail.com>
2024-10-02 10:09:08 +0200

    add fix

--- main.js
+++ main.js
@@ -7,16 +7,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 //stampare i numeri da 1 a 100
 for(let i = 1; i <= 100; i++){
-    console.log(i);   
+    if (i % 15 === 0) {
+        console.log('fizzbuzz');
+    }
 //se sono multipli di 3 stampi “Fizz”
-    if(i % 3 === 0){
+    else if(i % 3 === 0){
         console.log('fizz');
 //se sono multipli di 5 stampa "Buzz"
     }else if(i % 5 === 0){
         console.log('Buzz');
 //se sono sia multipli di 3 che di 5 stampi "FizzBuzz"
     }else{
-        console.log('fizzbuzz');   
+        console.log(i);   
     }
 }
 
